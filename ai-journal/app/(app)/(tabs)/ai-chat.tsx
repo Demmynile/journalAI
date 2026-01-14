1bd79db9e0aeaf5761afa47c59dfd9290e8cf157
demmynile <balogun80@gmail.com>
2026-01-14 10:16:07 +0100

    add new plan

---
+++ ai-journal/app/(app)/(tabs)/ai-chat.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function AiChat() {
+  return (
+    <View>
+      <Text>AiChat</Text>
+    </View>
+  )
+}
---
+++ ai-journal/app/(app)/(tabs)/journal.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function Journal() {
+  return (
+    <View>
+      <Text>Journal</Text>
+    </View>
+  )
+}
